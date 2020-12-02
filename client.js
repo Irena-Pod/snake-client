6d4ad7f4a517db7b2a85d1331de56dfe5c7e209b
Irena <irena.podgurski@gmail.com>
2020-12-02 19:07:36 +0000

    testing move commands

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 const net = require('net');
 
 //Establishes connection with the game server
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   //Event handler to handle incoming data
   conn.on('data', (data) => {
@@ -17,9 +17,19 @@ const connect = function() {
   //Event handler to handle our connection
   conn.on('connect', () => {
     console.log('Connection has been established successfully');
-    conn.write("Name: IP")
+    conn.write("Name: IP");
+    // hard coded move up command - testing
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+
+    
   });
+
+ 
+
   return conn;
 }
 
-module.exports = {connect};
+module.exports = { connect };
+
